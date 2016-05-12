6ac782d2d6ba968912563e54b5f8311c214840b8
bharding2 <b.harding@gmail.com>
2016-05-12 15:45:45 -0700

    in class updates HOLY FUCK

--- ben_harding/gulpfile.js
+++ ben_harding/gulpfile.js
@@ -2,30 +2,45 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
-// const exec = require('child_process').exec;
+const childProcess = require('child_process');
 const angularProtractor = require('gulp-angular-protractor');
-
-process.env.BUILD_PORT = 5025;
-const buildServer = require(__dirname + '/build_server');
 
 var apiFiles = ['./*.js', './lib/*.js', './models/*.js', './routes/*.js'];
 var specFiles = ['./test/**/*spec.js'];
 var testFiles = ['./test/**/*test.js'];
 var appFiles = ['./app/**/*.js'];
 
+var children = [];
+
+gulp.task('startservers:test', () => {
+  process.env.BUILD_PORT = 5525;
+  children.push(childProcess.fork('build_server.js'));
+  children.push(childProcess.spawn('mongod', ['--dbpath=./db']));
+  children.push(childProcess.fork('server.js', [], { env:
+    { MONGODB_URI: 'mongodb://localhost/slothbearTestDB' }
+  }));
+});
+
 gulp.task('webpack:dev', ['html:dev', 'css:dev'], () => {
   return gulp.src('app/js/entry.js')
     .pipe(webpack({
       output: {
+        devtool: 'source-map',
         filename: 'bundle.js'
       }
     }))
-    .pipe(gulp.dest('./build'))
-    .on('end', () => {
-      buildServer.close(() => {
-        console.log('build server close');
-      });
-    });
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('webpack:test', () => {
+  return gulp.src('test/unit/test_entry.js')
+    .pipe(webpack({
+      devtool: 'source-map',
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('./test'));
 });
 
 gulp.task('html:dev', () => {
@@ -43,13 +58,18 @@ gulp.task('test:mocha', () => {
     .pipe(mocha());
 });
 
-gulp.task('test:protractor', ['build:dev'], () => {
-  return gulp.src(['./test/integration/*spec.js'])
+gulp.task('test:protractor', ['startservers:test', 'build:dev'], () => {
+  return gulp.src(['./test/integration/**/*spec.js'])
     .pipe(angularProtractor({
       'configFile': './test/integration/config.js',
       'debug': true,
       'autoStartStopServer': true
-    }));
+    }))
+    .on('end', () => {
+      children.forEach((child) => {
+        child.kill('SIGTERM');
+      });
+    });
 });
 
 gulp.task('lint:api', () => {
--- ben_harding/test/integration/slothbear_spec.js
+++ ben_harding/test/integration/slothbear_spec.js
@@ -1,5 +1,10 @@
-describe('slothbear app', function() {
-  it('should have a 2 way data binding', function() {
-    browser.get('http://localhost:5575');
+describe('bears', function() {
+  it('should create a bear', function() {
+    browser.get('http://localhost:5525');
+    element(by.model('bearsctrl.newBear.name')).sendKeys('test bear');
+    element(by.id('createbear')).click();
+    element.all(by.css('#bearslist:first-child')).getText(function(text) {
+      expect(text).toEqual('test bear (gender: m) weighs 500 lbs and has a strength of 10');
+    });
   });
 });
