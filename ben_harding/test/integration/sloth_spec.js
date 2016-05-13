116c1637744f84dde959bbf9c26154ce054a1deb
bharding2 <b.harding@gmail.com>
2016-05-12 18:58:44 -0700

    first sloth e2e test

---
+++ ben_harding/test/integration/sloth_spec.js
@@ -0,0 +1,10 @@
+describe('sloths', function() {
+  it('should create a sloth', function() {
+    browser.get('http://localhost:5525');
+    element(by.model('slothsctrl.newSloth.name')).sendKeys('test sloth');
+    element(by.id('createsloth')).click();
+    element.all(by.css('#slothslist:first-child')).getText(function(text) {
+      expect(text).toEqual('test sloth (gender: f) weighs 100 lbs and has a strength of 50');
+    });
+  });
+});
